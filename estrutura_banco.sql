eca30e1d56f88a6a57fd113c23909b367287521c
Leonardo Correia <84795789+LeonardoCorreia08@users.noreply.github.com>
2026-09-06 02:57:20 -0300

    Add files via upload

--- estrutura_banco.sql
+++ estrutura_banco.sql
@@ -1,5 +1,5 @@
 /*
-	RELAÇÃO DE AUTORIZAÇÕES DOS SISTEMAS (login_autorizacao)
+    RELAÇÃO DE AUTORIZAÇÕES DOS SISTEMAS (login_autorizacao)
     ==============================================================
     Classificação das funcionalidades do sistemas que poderão ser 
     liberadas ou bloqueadas de acordo com os privilégios (autorizações)
@@ -12,19 +12,20 @@
     Relatório de contas a pagar
     etc...
 */
-create table login_autorizacao (
+CREATE TABLE login_autorizacao (
     id_autorizacao integer unsigned auto_increment,
     classificacao varchar(100),
     descricao varchar(200),
     ativo boolean default true,
     versao integer default 0,
+    criado_em timestamp default current_timestamp,
+    atualizado_em timestamp default current_timestamp on update current_timestamp,
     primary key (id_autorizacao)
 ) engine=innoDB;
 
 
-
 /*
-	PERFIL DE USUÁRIOS (login_perfil)
+    PERFIL DE USUÁRIOS (login_perfil)
     =======================================
     Grupo de usuários do sistema. 
     Podendo ser agrupados por setor, função, módulos, etc...
@@ -42,11 +43,13 @@ CREATE TABLE login_perfil (
   notificar_inclusao_usuario tinyint(4) DEFAULT '0',
   ativo tinyint(1) DEFAULT '1',
   versao int(11) DEFAULT '0',
+  criado_em timestamp default current_timestamp,
+  atualizado_em timestamp default current_timestamp on update current_timestamp,
   PRIMARY KEY (id_perfil)
 ) ENGINE=InnoDB;
 
 /*
-	OPERAÇÕES POR PERFIL (login_perfil_operacao)
+    OPERAÇÕES POR PERFIL (login_perfil_autorizacao)
     ============================================
     Relação das operações disponíveis por perfil (grupo) de usuários.
     Informação necessária na autorização das funcionalidades.
@@ -63,63 +66,63 @@ CREATE TABLE login_perfil (
     | etc...         | ...                                                 |
     +----------------+-----------------------------------------------------+
 */
-create table login_perfil_autorizacao (
+CREATE TABLE login_perfil_autorizacao (
     id_perfil integer unsigned not null,
     id_autorizacao integer unsigned not null,
     primary key (id_perfil, id_autorizacao)
 ) engine=innoDB;
 
-alter table login_perfil_autorizacao add constraint fk_perfil_autorizacao_perfil
-	foreign key (id_perfil) references login_perfil (id_perfil);
+ALTER TABLE login_perfil_autorizacao ADD CONSTRAINT fk_perfil_autorizacao_perfil
+    FOREIGN KEY (id_perfil) REFERENCES login_perfil (id_perfil) ON DELETE CASCADE;
 
-alter table login_perfil_autorizacao add constraint fk_perfil_autorizacao_autorizacao
-	foreign key (id_autorizacao) references login_autorizacao (id_autorizacao);
+ALTER TABLE login_perfil_autorizacao ADD CONSTRAINT fk_perfil_autorizacao_autorizacao
+    FOREIGN KEY (id_autorizacao) REFERENCES login_autorizacao (id_autorizacao) ON DELETE CASCADE;
 
 
 /*
-	RELAÇÃO DE USUÁRIOS (login_usuario)
+    RELAÇÃO DE USUÁRIOS (login_usuario)
     ========================================
     Usuários com autorização de uso do sistema.
 */
-
-create table login_usuario (
-	id_usuario integer unsigned auto_increment,
-    nome varchar(100),
-    login varchar(30),
-    email varchar(100) not null,
-    senha varchar(30) not null,
+CREATE TABLE login_usuario (
+    id_usuario integer unsigned auto_increment,
+    nome varchar(100) not null,
+    login varchar(30) not null unique,
+    email varchar(255) not null unique,
+    senha varchar(255) not null,
     ativo boolean default true,
     versao integer default 0,
+    criado_em timestamp default current_timestamp,
+    atualizado_em timestamp default current_timestamp on update current_timestamp,
     primary key (id_usuario)
 ) engine=innoDB;
 
 
 /*
-	USUÁRIOS POR PERFIL (login_usuario_perfil)
+    USUÁRIOS POR PERFIL (login_usuario_perfil)
     ==========================================
     Relação de usuários agrupados por perfil.
     Exemplo:
     +----------------+-----------------------------------------------------+
     | P E R F I L    | U S U Á R I O S                                     | 
     +----------------+-----------------------------------------------------+
-    | MASTER         | Leo, Bru                                       | 
+    | MASTER         | Leo, Bru                                            | 
     +----------------+-----------------------------------------------------+
     | ADMINISTRADOR  | Leonardo, Bruna, Art, ...                           |
     +----------------+-----------------------------------------------------+
-    | VENDEDOR       | Pamêlla, Maria, Lais, Nathalia, Thamy, ...            | 
+    | VENDEDOR       | Pamêlla, Maria, Lais, Nathalia, Thamy, ...          | 
     +----------------+-----------------------------------------------------+
     | etc...         | ...                                                 |
     +----------------+-----------------------------------------------------+
 */
-create table login_usuario_perfil (
+CREATE TABLE login_usuario_perfil (
     id_usuario integer unsigned,
     id_perfil integer unsigned,
     primary key (id_usuario, id_perfil)
 ) engine=innoDB;
 
-alter table login_usuario_perfil add constraint fk_usuario_perfil_usuario
-	foreign key (id_usuario) references login_usuario (id_usuario);
+ALTER TABLE login_usuario_perfil ADD CONSTRAINT fk_usuario_perfil_usuario
+    FOREIGN KEY (id_usuario) REFERENCES login_usuario (id_usuario) ON DELETE CASCADE;
 
-alter table login_usuario_perfil add constraint fk_usuario_perfil_perfil
-	foreign key (id_perfil) references login_perfil (id_perfil);
-
+ALTER TABLE login_usuario_perfil ADD CONSTRAINT fk_usuario_perfil_perfil
+    FOREIGN KEY (id_perfil) REFERENCES login_perfil (id_perfil) ON DELETE CASCADE;
